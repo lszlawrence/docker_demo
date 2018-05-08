084ced06584887c2d491beac2ca40fdac3c7a29a
Shizheng <lszlawrence@gmail.com>
2018-05-07 18:20:25 -0700

    change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var handleRequest = function(request, response) {
     console.log('Received request for URL: ' + request.url);
     response.writeHead(200);
-    response.end('Shizheng docker demo');
+    response.end('Applied AI Xian Xian De');
 };
 
 var www = http.createServer(handleRequest);
