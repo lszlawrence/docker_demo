02ae94381e7e07ffed86a30251b18dd033e93440
Shizheng <lszlawrence@gmail.com>
2018-05-08 11:15:58 -0700

    demo

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var handleRequest = function(request, response) {
     console.log('Received request for URL: ' + request.url);
     response.writeHead(200);
-    response.end('Suning Applied AI');
+    response.end('Suning Applied AI Xian Xian De');
 };
 
 var www = http.createServer(handleRequest);
